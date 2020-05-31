b9290ec8050151023c3c83d4d56afeee858eb446
2571796920@qq.com <2571796920@qq.com>
2020-05-31 21:30:07 +0800

    nacos服务发布

---
+++ nacos-provider/src/main/java/com/alibaba/cloud/nacos/service/EchoService.java
@@ -0,0 +1,9 @@
+package com.alibaba.cloud.nacos.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public interface EchoService {
+     @GetMapping(value = "/echo/{str}")
+     String echo(@PathVariable("str") String str);
+ }
