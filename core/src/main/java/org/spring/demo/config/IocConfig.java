571a73c32b3a8614ef69cd9e8143d328c0ac9811
BigBangz <2571796920@qq.com>
2024-04-04 20:51:39 +0800

    fix:注解包扫描使生效

--- core/src/main/java/org/spring/demo/config/IocConfig.java
+++ core/src/main/java/org/spring/demo/config/IocConfig.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.*;
 
 @Configuration
-@ComponentScan(basePackages = {"org.spring.demo.service", "org.spring.demo.util"})
+@ComponentScan(basePackages = {"org.spring.demo.service", "org.spring.demo.util", "org.spring.demo.annotation"})
 @PropertySource("app.properties")
 @EnableAspectJAutoProxy
 public class IocConfig {
